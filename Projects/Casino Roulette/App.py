f1e936f3256c02e4b3341cf86ee40d71103fdbda
mathis.dugue <mathis.dugue@etu.univ-tlse2.fr>
2025-06-03 15:23:33 +0200

    ajout de l'input, l'utilisateur peut choisir un nombre entre 0 et 36

--- Projects/Casino Roulette/App.py
+++ Projects/Casino Roulette/App.py
@@ -10,18 +10,31 @@ def get_color(number):
         return "Noir"
 
 # Fonction pour faire tourner la roulette
-def tourner_roulette():
+def tourner_roulette(pari):
     numero = random.randint(0, 36)
     couleur = get_color(numero)
     print(f"La bille est tombée sur : {numero} ({couleur})")
+    if numero == pari:
+        print("🎉 Félicitations ! Vous avez gagné !")
+    else:
+        print("😞 Perdu, réessayez !")
 
 # Lancer le programme
 if __name__ == "__main__":
     while True:
+        try:
+            pari = int(input("Choisissez un chiffre entre 0 et 36 : "))
+            if pari < 0 or pari > 36:
+                print("Veuillez entrer un chiffre valide entre 0 et 36.")
+                continue
+        except ValueError:
+            print("Entrée invalide. Veuillez entrer un chiffre.")
+            continue
+
         input("Appuie sur Entrée pour faire tourner la roulette...")
-        tourner_roulette()
+        tourner_roulette(pari)
+        
         rejouer = input("Voulez-vous rejouer ? (o/n) : ").lower()
         if rejouer != 'o':
             print("Merci d'avoir joué !")
             break
-
